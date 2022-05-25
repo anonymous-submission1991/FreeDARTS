8bea21fa19a530b3c9447befa58fd21d4893ff3e
anonymous-submission1991 <66350419+anonymous-submission1991@users.noreply.github.com>
2022-05-25 17:35:48 +0200

    Codes for mobilenet search space

---
+++ mobilenet_space/mobilenet/retrain_architecture/retrain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='-1 3 1 3 3 1 3 2 1 3 2 1 3 3 1 2 1 3 -1 3 1'
+
+python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='0 2 0 4 2 3 3 4 4 3 4 4 3 3 5 1 0 4 3 0 2'
+
+python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='5 4 0 1 2 5 3 0 0 3 0 4 4 0 4 2 4 5 4 0 1'
+
+
