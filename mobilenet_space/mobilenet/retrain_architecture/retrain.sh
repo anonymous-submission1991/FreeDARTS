b2321d961e72ef0f1296e15dbc7fe42ff0cf1e2a
anonymous-submission1991 <66350419+anonymous-submission1991@users.noreply.github.com>
2022-11-28 15:52:44 +0800

    Update retrain.sh

--- mobilenet_space/mobilenet/retrain_architecture/retrain.sh
+++ mobilenet_space/mobilenet/retrain_architecture/retrain.sh
@@ -5,4 +5,7 @@ python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_mo
 
 python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='5 4 0 1 2 5 3 0 0 3 0 4 4 0 4 2 4 5 4 0 1'
 
+python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='0 2 0 4 2 3 3 1 0 3 0 0 3 3 5 1 0 4 3 0 2'
+
+python -m torch.distributed.launch --nproc_per_node=1 retrain.py --save='eval_models' --model_id='1 0 0 2 2 1 3 1 0 3 1 4 3 3 5 1 4 5 0 1 3'
 
